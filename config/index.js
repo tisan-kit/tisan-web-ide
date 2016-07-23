6e11ae88ca96de9a8e47ccf19af64b80c27e9ce6
bugall <bugall@sina.cn>
2016-07-23 19:49:06 +0800

    change config module

--- config/index.js
+++ config/index.js
@@ -1,7 +1,17 @@
-var jsonconf = require("jsonconf");
-var path = require("path");
+var fs = require('fs');
 
-var f = path.join(__dirname,"./deploy.json");
-var  conf = jsonconf.parse(f);
-console.log(conf);
-module.exports = conf;
+var finalConfig={};
+if(fs.existsSync(__dirname+'/deploy.json')){
+  finalConfig = require('./depoy.json')
+    if(!finalConfig['env']&&finalConfig['env']==='development'){
+      finalConfig['env']='development';
+    }
+}else{
+  console.error('Can\'t find deploy config file in ./config');
+  process.exit(0);
+}
+
+console.log(finalConfig);
+
+module.exports=finalConfig;
+
